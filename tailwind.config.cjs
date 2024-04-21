d0691152bcf6938e00642ee9c69897ab3ed5a5bf
zstg <o0vckutt@duck.com>
2024-04-21 15:47:34 +0530

    Made JetBrains mono the default font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Roboto", "sans-serif", ...defaultTheme.fontFamily.sans],
+        sans: ["JetBrains Mono","Roboto", "sans-serif", ...defaultTheme.fontFamily.sans],
       },
     },
   },
