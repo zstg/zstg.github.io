439cfe08f8cda39c17d2a08c1f03c7e424d47be4
zstg <o0vckutt@duck.com>
2025-02-09 21:30:00 +0530

    Flake update

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      nativeBuildInputs = with pkgs; [ zola ];
+      shellHook = ''
+        zola serve
+      '';
+    };
+  };
+}
